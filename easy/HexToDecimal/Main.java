f86d3cb40c11d04bc2fb3d22d54299ea3fc21f8f
Alfredo Miranda <alfredo@miranda.io>
2014-12-22 13:24:20 -0600

    Write custom hex to decimal converted instead of using Integer.parseInt, it seems it's a little faster

--- easy/HexToDecimal/Main.java
+++ easy/HexToDecimal/Main.java
@@ -10,4 +10,41 @@ public class Main {
             System.out.println(Integer.parseInt(line, 16));
         }
     }
+
+    private static void hexToDecimal(String str) {
+        int num = 0;
+        int mul = 1;
+
+        for(int i = str.length() - 1; i >= 0; i--) {
+            char d = str.charAt(i);
+
+            switch(d) {
+                case 'a':
+                    num += 10 * mul;
+                    break;
+                case 'b':
+                    num += 11 * mul;
+                    break;
+                case 'c':
+                    num += 12 * mul;
+                    break;
+                case 'd':
+                    num += 13 * mul;
+                    break;
+                case 'e':
+                    num += 14 * mul;
+                    break;
+                case 'f':
+                    num += 15 * mul;
+                    break;
+                default:
+                    num += (d - 48) * mul;
+                    break;
+            }
+
+            mul *= 16;
+        }
+
+        System.out.println(num);
+    }
 }
